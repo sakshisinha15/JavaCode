5b20336f76a4462e42d8f64ff7ea5e3ff30b1f90
sakshisinha15 <sakshisinha2854@gmail.com>
2025-07-23 18:20:16 +0530

    Wrapper class

--- Wrapperclass.java
+++ Wrapperclass.java
@@ -1,8 +1,9 @@
-class Wrapperclass{
-    void fun1(){
-        
-    }
-    public static void main(String args[]){
-
+class Wrapperclass {
+    public static void main(String args[]) {
+        int x = Integer.parseInt("123");
+        Integer x1 = Integer.valueOf("10110", 2);
+        int y = x1.intValue();
+        System.out.println(x);
+        System.out.println(y);
     }
 }
