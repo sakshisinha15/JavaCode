7a1a589b666db1e17d1df4b05a82b93dea7fa886
sakshisinha15 <sakshisinha2854@gmail.com>
2025-07-24 14:44:45 +0530

    Package practice

---
+++ khushi.java
@@ -0,0 +1,11 @@
+package Khushi1;
+
+import Sakshi.*;
+
+public class khushi {
+    public static void main(String[] args) {
+        sakshi s = new sakshi();
+        s.setData(3, 4);
+        s.display();
+    }
+}
---
+++ sakshi.java
@@ -0,0 +1,15 @@
+package Sakshi;
+
+public class sakshi {
+    private int x;
+    private int y;
+
+    public void setData(int p, int q) {
+        x = p;
+        y = q;
+    }
+
+    public void display() {
+        System.out.println(x + y);
+    }
+}
