5c615803a9632788934385ffc47438fe91bdff95
sakshisinha15 <sakshisinha2854@gmail.com>
2025-07-25 15:03:25 +0530

    This and super keyword

--- thiss.java
+++ thiss.java
@@ -1,6 +1,6 @@
 class Example {
-    private int x;
-    private int y;
+    int x;
+    int y;
 
     void f1(int x, int y) {
         this.x = x;
@@ -11,15 +11,33 @@ class Example {
         // int x=2;
         // int y=3; if this we use then in sout we use this.x to print the value i.e
         // inside f1;
-        System.out.println(this.x);
-        System.out.println(this.y);
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
+
+class Example1 extends Example {
+    int x, y;
+
+    void f3(int x, int y) {
+        super.x = x;// parent wale ko assign karne ko
+        super.y = y;// parent wale ko assignkarne ko
+    }
+
+    void f4() {
+
+        System.out.println(super.x);
+        System.out.println(super.y);
     }
 }
 
 public class thiss {
     public static void main(String[] args) {
-        Example e = new Example();
-        e.f1(5, 4);
-        e.display();
+        // Example e = new Example();
+        // e.f1(5, 4);
+        // e.display();
+        Example1 e1 = new Example1();
+        e1.f3(7, 9);
+        e1.f4();
     }
 }
