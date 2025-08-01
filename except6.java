17e1f5899abdbd8649666c08b1e8b98e3ecd4d8d
sakshisinha15 <sakshisinha2854@gmail.com>
2025-08-01 21:37:37 +0530

    EXCEPTION HANDLING

---
+++ except6.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class except6 {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        System.out.println("A");
+        throw new IOException();
+    }
+
+}
